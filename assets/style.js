756624ab559355d656dde74e9fe84a76813b89d0
JayRodButray <jayrodbutray@gmail.com>
2023-05-23 16:45:42 -0700

    firstcommit,organized files, started adding code

---
+++ assets/style.js
@@ -0,0 +1,9 @@
+var triviaContainer = $("GameMode");
+var resultsContainer = $("BraggingResults");
+var finalTalley = $("Final Talley of YOUR Sports Knowledge");
+var sportsQuestions = [
+    {
+        question: ""
+    }
+]
+function createTriviaQuiz()

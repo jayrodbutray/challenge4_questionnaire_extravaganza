b4e8489ab8f00f92b951bc1de7840962d46d6d85
JayRodButray <jayrodbutray@gmail.com>
2023-05-26 15:50:02 -0700

    add functions for questions, revamp questions

--- assets/style.js
+++ assets/style.js
@@ -1,125 +1,110 @@
 var sportsQuestions = [
     {
         question: "Which NFL team won Super Bowl 50: The Golden Super Bowl?",
-        answers: {
-            a: "New England Patriots",
-            b: "Dallas Cowboys",
-            c: "Denver Broncos",
-            d: "Green Bay Packers"
-        },
-        correctAnswer: "c"
-    },
+        answers: ["New England Patriots", "Dallas Cowboys", "Denver Broncos", "Green Bay Packers"],
+        correctAnswer: "Denver Broncos"
+    },    
     {
         question: "Which NBA team is the only one to win two separate 3-Peats?",
-        answers: {
-            a: "Los Angeles Lakers",
-            b: "Boston Celtics",
-            c: "San Antonio Spurs",
-            d: "Chicago Bulls"
-        },
-        correctAnswer: "d"
+        answers: ["Los Angeles Lakers", "Boston Celtics", "San Antonio Spurs","Chicago Bulls"],
+        correctAnswer: "Chicago Bulls"
     },
     {
         question: "Which MLB team starred the pitching threat of John Smoltz, Tom Glavine, and Greg Maddoux?",
-        answers: {
-            a: "New York Yankees",
-            b: "Atlanta Braves",
-            c: "Boston Red Sox",
-            d: "New York Mets"
-        },
-        correctAnswer: "b"
+        answers: ["New York Yankees", "Atlanta Braves", "Boston Red Sox", "New York Mets"],
+        correctAnswer: "Atlanta Braves"
     },
-]
+];
 
-generateTriviaQuestions(sportsQuestions, triviaContainer, resultsContainer, startButton);
+let currentQuestion = 0;
+let talley = 0;
 
-function generateTriviaQuestions(questions, triviaContainer, resultsContainer, startButton){
-    function showQuestions (questions, triviaContainer){
-        var output = [];
-        var answers;
+function showQuestion(){
+    var questionEl = document.getElementById("question");
+    var answerEl = document.getElementById("answers");
+    var buttonEl = document.getElementById("button");
+    var finalTalleyEl = document.getElementById("finalresults");
 
-        for(var i=0; i<questions.length; i++){
-            answers = [];
+    var currentQuestionObject = questions[currentQuestion];
+    questionEl.textContent = currentQuestionObject.question;
 
-            for(letter in questions[i].answers){
+    answerEl.innerHTML = "";
+    currentQuestionObject.answers.forEach((answers, i) => {
+        var answerButton = document.createElement("button");
+        answerButton.textContent = answers;
+        answerButton.addEventListener("click", function(){
+            chosenAnswer(choice, finalTalleyEl);
+        });
 
+    answerEl.appendChild(answerButton);
+        });
+        submitButton.addEventListener("click", function(){
+            nextQuestion(finalTalleyEl);
+        });
+}
+
+function chosenAnswer(answer, finalTalleyEl){
+    var currentQuestionObject = questions[currentQuestion];
+    if(choice === currentQuestionObject.correctAnswer) {
+        finalTalleyEl.textContent = "Correct!";
+        talley++;
+    } else {
+        finalTalleyEl.textContent = "Incorrect!. The correct answer is: " + currentQuestionObject.correctAnswer;
     }
-}}
+}
 
 
-var triviaContainer = $("GameMode");
-var resultsContainer = $("BraggingResults");
-var startButton = $("startsportstrivia");
+function nextQuestion(finalTalleyEl) {
+    finalTalleyEl.textContent = "";
+    currentQuestion++;
 
-console.log(startButton);
+    if(currentQuestion ===questions.length){
+        showFinalTalley();
+    }else{
+        displayQuestion();
+    }
+}
+
+function showFinalTalley(){
+    var quizContainer = document.getElementById("Quiz-box");
+    quizContainer.innerHTML = "";
+
+    var finalScoreEl = document.createElement("h1");
+    finalScoreEl.textContent = "Quiz is complete. Your Score: " + score + "out of" + questions.length;
+    quizContainer.appendChild(finalScoreEl);
+}
+
+displayQuestion
 
 
+for (let i = 0; i < sportsQuestions.length; i++) {
+    writeQuestion(questions[i]);
+    
+}
 
+function writeQuestion(questionObj){
+    var question = questionObj.question;
+    var answers = questionObj.answers;
+    var correctAnswer = questionObj.correctAnswer;
+    
+    console.log(question);
+    for (let i = 0; i < answers.length; i++) {
+        console.log((i + 1) + "." + answers[i]);
+        
+    }
 
+    var choice = prompt("Select the correct choice:");
+    var selectedAnswer = answers[choice - 1];
 
-var gameStarted = false;
-var timerElement = $("Timer");
-var generateTriviaQuestions
-var answers;
-
-var finalTalley = $("Final Talley of YOUR Sports Knowledge");
-
-
-
-startButton.on("click", function(){
-    triviaContainer.style.display = "block";
-    gameStarted = true;
-
-    renderGuessToBrowser();
-    renderTimerToBrowser();
-    var interval = setInterval(function(){
-        if (gameStarted){
-            return;
+    if (selectedAnswer === correctAnswer) {
+        console.log("Correct");
+        } else {
+            console.log("Incorrect. The correct answer is:", correctAnswer);
         }
-        timerElement --;
-        if (timerElement <= 0){
-            clearInterval(interval);
-        }
-        renderTimerToBrowser();
-    }, 1000);
+    };
 
+var button = document.getElementById("button");
+
+button.addEventListener("click", function() {
+    console.log("button clicked!");
 });
-
-function renderGuessToBrowser(){
-    triviaContainer.textContent = guess;
-}
-
-function renderTimerToBrowser(){
-    timerHolder.textContent = "Timer left" + timerElement;
-}
-var sportsQuestions = [
-    {
-        question: "Which NFL team won Super Bowl 50: The Golden Super Bowl?",
-        answers: {
-            a: "New England Patriots",
-            b: "Dallas Cowboys",
-            c: "Denver Broncos",
-            d: "Green Bay Packers"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Which NBA team is the only one to win two separate 3-Peats?",
-        answers: {
-            a: "Los Angeles Lakers",
-            b: "Boston Celtics",
-            c: "San Antonio Spurs",
-            d: "Chicago Bulls"
-        },
-        correctAnswer: "d"
-    },
-    {
-        question: "Which MLB team starred the pitching threat of John Smoltz, Tom Glavine, and Greg Maddoux?",
-        answers: {
-            a: "New York Yankees",
-            b: "Atlanta Braves",
-            c: "Boston Red Sox",
-            d: "New York Mets"
-        },
-        correctAnswer: "b"
-    }]}
